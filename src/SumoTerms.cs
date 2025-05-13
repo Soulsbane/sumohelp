eafd902cc8dc254eb485b131c826b06583c5e717
Paul Crane <paul@acheronsoft.com>
2025-05-13 09:10:07 -0500

    Add initial class SumoTermLoader

--- src/SumoTerms.cs
+++ src/SumoTerms.cs
@@ -1,3 +1,23 @@
+using System.Text.Json;
+
 namespace sumohelp;
 
 public record SumoTerms(IDictionary<string, string> Terms);
+
+class SumoTermLoader
+{
+	private SumoTerms? terms;
+
+	public void Load(string jsonFilePath)
+	{
+		using var stream = new FileStream(jsonFilePath, FileMode.Open, FileAccess.Read);
+		var options = new JsonSerializerOptions
+		{
+			PropertyNameCaseInsensitive = true,
+			ReadCommentHandling = JsonCommentHandling.Skip,
+			AllowTrailingCommas = true
+		};
+
+		terms = JsonSerializer.Deserialize<SumoTerms>(stream, options) ?? throw new InvalidOperationException("Failed to load terms.");
+	}
+}
