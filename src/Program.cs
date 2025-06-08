cf2b947e0b408de0f440cba0851fd4eef2f5cbd5
Paul Crane <paul@acheronsoft.com>
2025-06-07 23:55:49 -0500

    Add initial command line help

--- src/Program.cs
+++ src/Program.cs
@@ -1,39 +1,89 @@
 ﻿
+using System.ComponentModel;
 using Spectre.Console;
+using Spectre.Console.Cli;
 
-if (args.Length > 0)
+
+var app = new CommandApp<GetTermCommand>();
+
+app.Configure(config =>
 {
-	GetTerm(args[0]);
+	config.AddCommand<ListCommand>("list")
+		.WithDescription("List all the terms from the local help term database")
+		.WithExample(new[] { "list", "term" });
+});
+
+try
+{
+	return app.Run(args);
 }
-else
+catch (Exception ex)
 {
-	Console.WriteLine("No arguments provided.");
+	AnsiConsole.WriteException(ex, ExceptionFormats.ShortenEverything);
+	return -1;
 }
 
-void GetTerm(string termToFind)
+
+
+public class GetTermCommandSettings : CommandSettings
 {
-	var termLoader = new SumoHelp.SumoTermLoader();
-	string? definition = termLoader.FindExact(termToFind);
+	[Description("Term to search for in the terms database")]
+	[CommandArgument(0, "[term]")]
+	public string? Term { get; init; } = string.Empty;
+}
 
-	if (definition != null)
+public class GetTermCommand : Command<GetTermCommandSettings>
+{
+	void GetTerm(string termToFind)
 	{
-		Console.WriteLine($"Definition for {termToFind} is: {definition}");
-	}
-	else
-	{
-		var searchResults = termLoader.FindAll(termToFind);
+		var termLoader = new SumoHelp.SumoTermLoader();
+		string? definition = termLoader.FindExact(termToFind);
 
-		if (searchResults.Count > 0)
+		if (definition != null)
 		{
-			var choice = AnsiConsole.Prompt(new SelectionPrompt<string>()
-				.Title("No exact match found. Do you want to search for similar terms?")
-				.AddChoices(searchResults.Keys));
-
-			 	Console.WriteLine($"{searchResults[choice]}");
+			Console.WriteLine($"Definition for {termToFind} is: {definition}");
 		}
 		else
 		{
-			Console.WriteLine("No similar terms found.");
+			var searchResults = termLoader.FindAll(termToFind);
+
+			if (searchResults.Count > 0)
+			{
+				var choice = AnsiConsole.Prompt(new SelectionPrompt<string>()
+					.Title("No exact match found. Do you want to search for similar terms?")
+					.AddChoices(searchResults.Keys));
+
+				Console.WriteLine($"{searchResults[choice]}");
+			}
+			else
+			{
+				Console.WriteLine("No similar terms found.");
+			}
 		}
+	}
+
+	public override int Execute(CommandContext context, GetTermCommandSettings settings)
+	{
+		if (string.IsNullOrWhiteSpace(settings.Term))
+		{
+			Console.WriteLine("Please provide a term to search for.");
+			return -1;
+		}
+
+		GetTerm(settings.Term);
+		return 0;
+	}
+}
+
+public class ListCommandSettings : CommandSettings
+{
+}
+
+public class ListCommand : Command<ListCommandSettings>
+{
+	public override int Execute(CommandContext context, ListCommandSettings settings)
+	{
+		Console.WriteLine("LISTING.");
+		return 0;
 	}
 }
