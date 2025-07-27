0aade85936c5f96d25605f46cf64706e9e5f9c2e
Paul Crane <paul@acheronsoft.com>
2025-07-27 16:36:20 -0500

    Move GetTermCommand and GetTermCommandSettings to their own file

---
+++ src/Commands/TermCommand.cs
@@ -0,0 +1,62 @@
+namespace SumoHelp.Commands;
+
+using System.ComponentModel;
+using Spectre.Console;
+using Spectre.Console.Cli;
+
+public class GetTermCommandSettings : CommandSettings
+{
+	[Description("Term to search for in the terms database")]
+	[CommandArgument(0, "[term]")]
+	public string? Term { get; init; } = string.Empty;
+}
+
+public class GetTermCommand : Command<GetTermCommandSettings>
+{
+	void GetTerm(string termToFind)
+	{
+		var termLoader = new SumoHelp.SumoTermLoader();
+		string definition = termLoader.FindExact(termToFind);
+
+		if (definition != string.Empty)
+		{
+			Console.WriteLine($"Definition for {termToFind} is: {definition}");
+		}
+		else
+		{
+			var searchResults = termLoader.FindAll(termToFind);
+
+			if (searchResults.Count > 0)
+			{
+				if (searchResults.Count == 1)
+				{
+					Console.WriteLine($"{searchResults.First().Value}");
+				}
+				else
+				{
+					var choice = AnsiConsole.Prompt(new SelectionPrompt<string>()
+						.Title("No exact match found. Do you want to search for similar terms?")
+						.AddChoices(searchResults.Keys));
+
+					Console.WriteLine($"{searchResults[choice]}");
+				}
+			}
+			else
+			{
+				Console.WriteLine("No similar terms found.");
+			}
+		}
+	}
+
+	public override int Execute(CommandContext context, GetTermCommandSettings settings)
+	{
+		if (string.IsNullOrWhiteSpace(settings.Term))
+		{
+			Console.WriteLine("Please provide a term to search for.");
+			return -1;
+		}
+
+		GetTerm(settings.Term);
+		return 0;
+	}
+}
--- src/Program.cs
+++ src/Program.cs
@@ -1,9 +1,7 @@
 ﻿
-using System.ComponentModel;
 using Spectre.Console;
 using Spectre.Console.Cli;
 using SumoHelp.Commands;
-
 
 var app = new CommandApp<GetTermCommand>();
 
@@ -22,62 +20,5 @@ catch (Exception ex)
 {
 	AnsiConsole.WriteException(ex, ExceptionFormats.ShortenEverything);
 	return -1;
-}
-
-public class GetTermCommandSettings : CommandSettings
-{
-	[Description("Term to search for in the terms database")]
-	[CommandArgument(0, "[term]")]
-	public string? Term { get; init; } = string.Empty;
-}
-
-public class GetTermCommand : Command<GetTermCommandSettings>
-{
-	void GetTerm(string termToFind)
-	{
-		var termLoader = new SumoHelp.SumoTermLoader();
-		string definition = termLoader.FindExact(termToFind);
-
-		if (definition != string.Empty)
-		{
-			Console.WriteLine($"Definition for {termToFind} is: {definition}");
-		}
-		else
-		{
-			var searchResults = termLoader.FindAll(termToFind);
-
-			if (searchResults.Count > 0)
-			{
-				if (searchResults.Count == 1)
-				{
-					Console.WriteLine($"{searchResults.First().Value}");
-				}
-				else
-				{
-					var choice = AnsiConsole.Prompt(new SelectionPrompt<string>()
-						.Title("No exact match found. Do you want to search for similar terms?")
-						.AddChoices(searchResults.Keys));
-
-					Console.WriteLine($"{searchResults[choice]}");
-				}
-			}
-			else
-			{
-				Console.WriteLine("No similar terms found.");
-			}
-		}
-	}
-
-	public override int Execute(CommandContext context, GetTermCommandSettings settings)
-	{
-		if (string.IsNullOrWhiteSpace(settings.Term))
-		{
-			Console.WriteLine("Please provide a term to search for.");
-			return -1;
-		}
-
-		GetTerm(settings.Term);
-		return 0;
-	}
 }
 
