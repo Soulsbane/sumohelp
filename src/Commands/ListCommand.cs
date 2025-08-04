a64779ab5a99fea218e911f014b51be1c35df56d
Paul Crane <paul@acheronsoft.com>
2025-08-03 22:18:46 -0500

    Add initial support for passing an argument to 'list' command

--- src/Commands/ListCommand.cs
+++ src/Commands/ListCommand.cs
@@ -1,17 +1,31 @@
 namespace SumoHelp.Commands;
 
+using System.ComponentModel;
 using Spectre.Console;
 using Spectre.Console.Cli;
 
 public class ListCommandSettings : CommandSettings
 {
+	[Description("List of terms to display from the local help term database. If not specified, all terms will be listed.")]
+	[CommandArgument(0, "[listType]")]
+	public string ListType { get; init; } = string.Empty;
 }
 
 public class ListCommand : Command<ListCommandSettings>
 {
 	public override int Execute(CommandContext context, ListCommandSettings settings)
 	{
-		var termLoader = new SumoHelp.SumoTermLoader();
+		if (settings.ListType == "all" || string.IsNullOrWhiteSpace(settings.ListType))
+		{
+			return ListAllTerms();
+		}
+
+		return 0;
+	}
+
+	private int ListAllTerms()
+	{
+		var termLoader = new SumoTermLoader();
 		var allTerms = termLoader.GetAll();
 
 		if (allTerms.Count == 0)
